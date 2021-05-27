e58efc85f2816b0f3b0bd2043f5526cfb4b534ef
kaki <suka6411@gmail.com>
2021-05-27 20:08:42 +0900

    feat: ディレクトリの中身を取得する処理を書いた

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
 
-int main(void) {
-    printf("Hello, world\n");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s dir\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    DIR *dp = opendir(argv[1]);
+
+    struct dirent* dirp;
+    while ((dirp = readdir(dp)) != NULL) {
+        printf("%s\n", dirp->d_name);
+    }
+    closedir(dp);
+
     return 0;
 }
