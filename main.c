fe51d7f9473e2db760786d80bda2d3b5adfcf506
kaki <suka6411@gmail.com>
2021-05-28 22:06:33 +0900

    feat: バージョン情報の表示を実装

--- main.c
+++ main.c
@@ -7,19 +7,27 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include "tree.h"
+#include "config.h"
 
 int help = 0;
+int version = 0;
 
 void show_usage(const char *progname) {
     printf("Usage: %s [OPTION]... DIR\n", progname);
     printf("list contents of directories in a tree-like format\n");
     printf("\n");
     printf("     --help     print this message and exit\n");
+    printf("     --version  output version infomation and exit\n");
+}
+
+void show_version() {
+    printf("tree %d.%d\n", TREE_VERSION_MAJOR, TREE_VERSION_MINOR);
 }
 
 int main(int argc, char *argv[]) {
     struct option longopts[] = {
-        { "help", no_argument, &help, 1 },
+        { "help",    no_argument, &help,    1 },
+        { "version", no_argument, &version, 1 },
     };
 
     if (argc != 2) {
@@ -33,6 +41,11 @@ int main(int argc, char *argv[]) {
 
     if (help) {
         show_usage(argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+
+    if (version) {
+        show_version(argv[0]);
         exit(EXIT_SUCCESS);
     }
 
