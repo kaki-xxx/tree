d7baf5f5fac846feb04f7af2442a9ea46086f166
kaki <suka6411@gmail.com>
2021-05-28 21:56:21 +0900

    feat: ヘルプメッセージの表示を実装

--- main.c
+++ main.c
@@ -3,12 +3,37 @@
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include "tree.h"
 
+int help = 0;
+
+void show_usage(const char *progname) {
+    printf("Usage: %s [OPTION]... DIR\n", progname);
+    printf("list contents of directories in a tree-like format\n");
+    printf("\n");
+    printf("     --help     print this message and exit\n");
+}
+
 int main(int argc, char *argv[]) {
+    struct option longopts[] = {
+        { "help", no_argument, &help, 1 },
+    };
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s dir\n", argv[0]);
         exit(EXIT_FAILURE);
+    }
+
+    char opt;
+    int longindex;
+    while ((opt = getopt_long(argc, argv, "", longopts, &longindex)) != -1) {}
+
+    if (help) {
+        show_usage(argv[0]);
+        exit(EXIT_SUCCESS);
     }
 
     if (do_tree(argv[1]) < 0) {
